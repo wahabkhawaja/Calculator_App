f4b43b8f94c6512cf5338463b0acc6757450b98d
wahab <wahabrider984@gmail.com>
2023-06-21 15:41:07 -0700

    calculator app

---
+++ lib/constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor= Colors.white;
+const Color yellowColor= Colors.yellow;
+
+const headingTextStyle =TextStyle(
+  fontSize: 30,
+  fontWeight:FontWeight.bold,
+  color: yellowColor
+);
